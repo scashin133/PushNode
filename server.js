e8dd9feb187da3d5abd078341665f0498179541c
Van Miranda <van@socialcast.com>
2010-10-01 22:03:22 -0700

    Wrap JSON.parse and body listener in a check for method == POST

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ var events = require('events');
 var sys = require('sys');
 var port = 8098;
 http.createServer(function (request, response) {
-  var body = "";
   
-  request.addListener('data', function(chunk){
-    body += chunk;
-  });
+  if (request.method == "POST"){
+    var body = "";
+
+    request.addListener('data', function(chunk){
+      body += chunk;
+    });
+
+    request.addListener('end', function(){
+      console.log(JSON.parse(body));
+    });
+  }
   
-  request.addListener('end', function(){
-    console.log(JSON.parse(body));
-  });
 }).listen(port);
 console.log('Server running at http://localhost:' + port + '/');
